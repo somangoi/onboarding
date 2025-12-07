1ffe607f1eca84820090902601799037949bf88c
Somi Hwang <somi.c.hwang@gmail.com>
2025-12-07 22:35:06 +0900

    fix: 버튼 css 이슈 수정

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -14,7 +14,7 @@ export function Button({
   variant?: ButtonVariant;
   className?: string | undefined;
 }) {
-  const baseStyles = "px-8 py-3 rounded-full text-[var(--font-size-button)] font-[var(--font-weight-button)] transition-all duration-200  disabled:cursor-not-allowed cursor-pointer";
+  const baseStyles = "px-8 py-3 rounded-full text-[var(--font-size-button)] font-[var(--font-weight-button)] disabled:cursor-not-allowed cursor-pointer";
 
   const variantStyles = {
     primary: "bg-[var(--color-primary)] text-white hover:bg-[var(--color-primary)] active:scale-95 min-w-[200px] cursor-pointer",
